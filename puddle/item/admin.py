830ce0ce70f2bc37b36f4b8d640cc979e77a752b
roscibely <rosanacibely@gmail.com>
2023-07-06 21:13:12 -0300

    feat: added item

--- puddle/item/admin.py
+++ puddle/item/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Item
+
+admin.site.register(Category)
+admin.site.register(Item)
+# This code imports the admin module and the Category model from the item application.
+# The last line registers the Category model with the admin site.
+
+
+
--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Item
 
 # Create your views here.
+
+def detail(request, item_id):
+    
+    item = get_object_or_404(Item, pk=item_id)
+
+    return render(request, 'item/detail.html', {'item': item})
+
